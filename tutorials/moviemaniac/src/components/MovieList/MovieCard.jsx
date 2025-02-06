e9eabaf61232fef60cab66a65fb4bdaf5b711557
bpcayton <1620814268121002@mil>
2025-02-06 17:55:41 -0500

    continued training

---
+++ tutorials/moviemaniac/src/components/MovieList/MovieCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./MovieCard.css";
+import Star from "../../assets/star.png";
+import AppConfig from "../../config/config.json";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <a href="" className="movie_card">
+      <img
+        src={AppConfig.tmdbImagePath + movie.poster_path}
+        alt="movie poster"
+        className="movie_post"
+      />
+      <div className="movie_details">
+        <h3 className="movie_details_heading">{movie.original_title}</h3>
+        <div className="align_center movie_date_rate">
+          <p>{movie.release_date}</p>
+          <p>
+            {movie.vote_average}{" "}
+            <img src={Star} alt="rating icon" className="card_emoji" />
+          </p>
+        </div>
+        <p className="movie_description">
+          {movie.overview.slice(0, 100) + "..."}
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export default MovieCard;
--- tutorials/moviemaniac/src/components/MovieList/MovieList.jsx
+++ tutorials/moviemaniac/src/components/MovieList/MovieList.jsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
+import MovieCard from "./MovieCard";
+import Config from "../../config/config.json";
 
 const MovieList = () => {
+  const api_key = Config.api_key;
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  // Fetch is cleaner than a bunch of chained .then methods
+  const fetchMovies = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`
+    );
+    const data = await response.json();
+    setMovies(data.results);
+  };
+
   return (
     <section className="movie_list">
-      <header className="movie_list_header">
-        <h2 className="movie_list_heading">
-          Popular <img src={Fire} alt="fire emoji" />
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
+          Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
         </h2>
+        <div className="align_center movie_list_fs">
+          <ul className="align_center movie_filter">
+            <li className="movie_filter_item active">8+ Star</li>
+            <li className="movie_filter_item">7+ Star</li>
+            <li className="movie_filter_item">6+ Star</li>
+          </ul>
+
+          <select name="" id="" className="movie_sorting">
+            <option value="">SortyBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
+          </select>
+          <select name="" id="" className="movie_sorting">
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
+          </select>
+        </div>
       </header>
+      <div className="movie_cards">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </section>
   );
 };
